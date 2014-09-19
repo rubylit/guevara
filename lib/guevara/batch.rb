1955f5f36ce4f26dc9a4670a0e5ead66b2aadfae
Eloy Espinaco <eloyesp@gmail.com>
2014-09-19 10:48:05 -0300

    Generates entry details.

--- lib/guevara/batch.rb
+++ lib/guevara/batch.rb
@@ -3,16 +3,27 @@ require 'date'
 module Guevara
   class Batch
 
+    ACCOUNT_TYPE_CODE     = { 'checking' => '2', 'saving' => '3' }
+    TRANSACTION_TYPE_CODE = { 'credit' => '2', 'debit' => '7' }
+
     attr_reader :service_class, :options, :transactions
-    
+
     def initialize(transactions, options)
       @service_class  = 200
       @options        = options
-      @transactions   = transactions      
+      @transactions   = transactions
     end
-  
+
     def to_s
-      format "5%3d%-16.16s%-20.20s%10.10sPPD          %s%s   1%8d%07d\n",
+      batch = [ batch_header ]
+      transactions.each_with_index do |transaction, index|
+        batch << entry_detail(transaction, index)
+      end
+      batch.join("\n")
+    end
+
+    def batch_header
+      format "5%3d%-16.16s%-20.20s%10.10sPPD          %s%s   1%8d%07d",
         service_class,
         options[:company_name],
         '', # company discretionary data
@@ -21,10 +32,26 @@ module Guevara
         effective_date.strftime("%y%m%d"),
         options[:routing_number],
         options[:index]
-    end  
-    
+    end
+
+    def entry_detail transaction, index
+      transaction_code =
+        ACCOUNT_TYPE_CODE[transaction[:account_type]] +
+        TRANSACTION_TYPE_CODE[transaction[:type]]
+      format "6%2d%9d%-17.17s%010d%-15.15s%-22.22s  1%8d%07d",
+        transaction_code,
+        transaction[:routing_number],
+        transaction[:account_number],
+        transaction[:amount],
+        transaction[:id],
+        "#{ transaction[:first_name] } #{ transaction[:last_name] }",
+        options[:routing_number],
+        index + 1
+    end
+
     def effective_date
       Date.parse transactions.first[:effective_date]
-    end  
-  end  
+    end
+
+  end
 end
--- test/batch_generation_test.rb
+++ test/batch_generation_test.rb
@@ -1,9 +1,10 @@
 require_relative '../lib/guevara/batch'
+
 def sample_transaction
   {
     id:               'FD00AFA8A0F7',
     type:             'debit',
-    amount:           '1600',
+    amount:           1600,
     effective_date:   '2014-09-21',
     first_name:       'marge',
     last_name:        'baker',
@@ -14,7 +15,7 @@ def sample_transaction
     telephone:        '5858232966',
     account_type:     'checking',
     routing_number:   '103100195',
-    account_number:   '3423423253234'
+    account_number:   '3ACCOUNT234'
   }
 end
 
@@ -28,9 +29,30 @@ def sample_options
   }
 end
 
+def debugger_equal out, expected
+  puts
+  puts <<NACHA
+# #{ %w[ 0 1 2 3 4 5 6 7 8 9 ].map { |i| i.to_s * 10 }.join('')[1..-1] }
+# #{ '1234567890' * 10 }
+O #{ out }
+E #{ expected }
+#
+NACHA
+  assert_equal out, expected
+end
+
 test 'generates the batch header' do
   batch = Guevara::Batch.new [ sample_transaction ], sample_options
   assert_equal batch.to_s.lines.to_a.first, <<NACHA
 5200rubylit                                Ruby123PPD          140918140921   1123456780000001
 NACHA
+end
+
+test 'generates the entry detail record' do
+  batch = Guevara::Batch.new [ sample_transaction ], sample_options
+  entry_detail = batch.to_s.lines.to_a[1]
+  expected =
+"6271031001953ACCOUNT234      0000001600FD00AFA8A0F7   marge baker             1123456780000001"
+
+  assert_equal entry_detail, expected
 end
