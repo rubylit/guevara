543fe9cc06a7af832b7ce859d163f119ef28877c
Eloy Espinaco <eloyesp@gmail.com>
2014-09-23 17:34:09 -0300

    Batch is generated row by row.

--- lib/guevara/batch.rb
+++ lib/guevara/batch.rb
@@ -1,76 +1,50 @@
+require_relative 'batch_header'
+require_relative 'batch_control'
+require_relative 'addenda'
 require_relative 'entry'
 require 'date'
 
 module Guevara
   class Batch
 
-    ACCOUNT_TYPE_CODE     = { 'checking' => '2', 'saving' => '3' }
-    TRANSACTION_TYPE_CODE = { 'credit' => '2', 'debit' => '7' }
-
-    attr_reader :attributes, :transactions
+    attr_reader :transactions, :attributes
 
     def initialize(transactions, attributes)
       @transactions = transactions
-      self.attributes  = attributes
-    end
-
-    def attributes= attributes
-      default_attributes = {
-        discretionary_data: nil,
-        effective_date:     effective_date.strftime("%y%m%d")
-      }
-      @attributes = default_attributes.merge(attributes)
-      @attributes[:date] = Date.parse(attributes[:date]).strftime("%y%m%d")
+      @attributes   = attributes
     end
 
     def to_s
-      batch = [ batch_header ]
-      entries.each do |entry|
-        batch << entry.to_s
+      batch = []
+      batch << batch_header.to_s
+      transactions.each_with_index do |transaction, index|
+        batch << entry(transaction, index).to_s
+        batch << addenda(transaction, index).to_s
       end
       batch << batch_control
       batch.join
     end
 
     def batch_header
-      format ["5",
-              "%<service_class>3d",
-              "%<company_name>-16.16s",
-              "%<discretionary_data>-20.20s",
-              "%<company_id>10.10s",
-              "PPD          ",
-              "%<date>s%<effective_date>s",
-              "   1",
-              "%<routing_number>8d",
-              "%<index>07d",
-              "\n"].join, attributes
+      header_attributes = { effective_date: effective_date }.merge(attributes)
+      BatchHeader.new header_attributes
     end
 
     def batch_control
-      attributes[:entry_addenda_count] = entries.size
-      attributes[:entry_hash]          = entry_hash
-      attributes[:total_debit]         = total('debit')
-      attributes[:total_credit]        = total('credit')
-      format ["8",
-              "%<service_class>3d",
-              "%<entry_addenda_count>06d",
-              "%<entry_hash>010d",
-              "%<total_debit>012d",
-              "%<total_credit>012d",
-              "%<company_id>10.10s",
-              " " * 25,
-              "%<routing_number>8d",
-              "%<index>07d",
-              "\n"].join, attributes
+      control_attributes = {
+        entry_count:    transactions.size * 2,
+        entry_hash:     entry_hash,
+        total_debit:    total('debit'),
+        total_credit:   total('credit')
+      }.merge(attributes)
+      BatchControl.new control_attributes
     end
 
     def entry_hash
-      entries.
-        map{ |e| e.to_s }.
-        select{ |e| e[0] == '6' }. # only use entry detail rows
-        map{ |e| e[3..10].to_i }.  # take only 4-11 places
-        reduce(:+).                # sum
-        modulo(10_000_000_000)     # cap to 10 digits
+      transactions.
+        map{ |t| t[:routing_number].to_i / 10 }. # ignore the check digit
+        reduce(:+).                              # sum
+        modulo(10_000_000_000)                   # cap to 10 digits
     end
 
     def total type
@@ -82,15 +56,17 @@ module Guevara
     end
 
     def effective_date
-      Date.parse transactions.first[:effective_date]
+      transactions.first[:effective_date]
     end
 
-    def entries
-      @entries ||= transactions.each_with_index.map do |transaction, index|
-        t = transaction.merge index: index + 1,
-                              signature_routing_number: attributes[:routing_number]
-        Entry.new t
-      end
+    def entry transaction, index
+      entry_attributes = transaction.merge(number: index + 1,
+                                           origin_id: attributes[:origin_id])
+      Entry.new entry_attributes
+    end
+
+    def addenda transaction, index
+      Addenda.new transaction.merge(entry_number: index + 1)
     end
 
   end
---
+++ test/test_batch_generation.rb
@@ -0,0 +1,24 @@
+require_relative 'helper.rb'
+require_relative '../lib/guevara/batch'
+
+setup do
+  Guevara::Batch.new [ sample_transaction ],
+    service_class:  '200',
+    company_name:   'rubylit',
+    company_id:     'Ruby123',
+    company_date:   '2014-09-18',
+    origin_id:      12345678,
+    number:         1
+end
+
+test 'generates the batch' do |batch|
+  expected = <<NACHA
+5200rubylit                                Ruby123PPD          140918140921   1123456780000001
+6271031001953ACCOUNT234      0000001600FD00AFA8A0F7   marge baker             1123456780000001
+705wellsville|KS|66092|101 2nd st|                                                 00010000001
+82000000020010310019000000001600000000000000   Ruby123                         123456780000001
+NACHA
+  batch.to_s.lines.to_a.each_with_index do |line, index|
+    debugger_equal line, expected.lines.to_a[index]
+  end
+end
