97f5f51aaae80677ca08077c9cfcd06f2690a7e0
Eloy Espinaco <eloyesp@gmail.com>
2014-10-06 09:02:58 -0300

    Use sample batch for batch header test.

--- test/test_batch_header.rb
+++ test/test_batch_header.rb
@@ -2,23 +2,14 @@ require_relative 'helper.rb'
 require_relative '../lib/guevara/batch_header'
 
 setup do
-  {
-    service_class:     '200',
-    company_name:      'rubylit',
-    company_id:        '7654321',
-    entry_description: '7166666666',
-    company_date:      '2014-09-18',
-    effective_date:    '2014-09-21',
-    origin_id:         '12345678',
-    number:            1
-  }
+  sample_batch.merge(number: 1)
 end
 
 test 'generates the batch header' do |attributes|
   batch_header = Guevara::BatchHeader.new attributes
 
   debugger_equal batch_header.to_s, <<NACHA
-5200rubylit                                7654321PPD7166666666140918140921   1123456780000001
+5200rubylit                                Ruby123PPD7166666666140918140921   1123456780000001
 NACHA
 end
 
@@ -27,6 +18,6 @@ test 'discretionary data can be added' do |attributes|
     discretionary_data: 'something here')
 
   debugger_equal batch_header.to_s, <<NACHA
-5200rubylit         something here         7654321PPD7166666666140918140921   1123456780000001
+5200rubylit         something here         Ruby123PPD7166666666140918140921   1123456780000001
 NACHA
 end
